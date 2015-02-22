fdc92831c9e3d86b71c0e7ea086ec2bc102baa6a
WenLiZhuang <r03922101@csie.ntu.edu.tw>
2015-02-22 23:08:44 +0800

    fix ssh config

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 
 list=$( cat <<DOC
-  ssh.conf  $HOME/.ssh/config
-  tmux.conf $HOME/.tmux.conf
-  zshrc     $HOME/.zshrc
-  vimrc     $HOME/.vimrc
+  ssh.config  $HOME/.ssh/config
+  tmux.conf   $HOME/.tmux.conf
+  zshrc       $HOME/.zshrc
+  vimrc       $HOME/.vimrc
 
 DOC
 )
